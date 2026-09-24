14037584f26f9bd3549be3b5db30934b239c80f8
John <johnh.dk93@gmail.com>
2026-09-24 18:50:25 +0200

    Add project files.

---
+++ HarmonyPatches.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using LazyBearTechnology;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllerIconSwapper
+{
+	[HarmonyPatch]
+	public static class HarmonyPatches
+	{
+		[HarmonyPatch(typeof(ControllerIconLibrary), "UpdateInputDeviceForPC")]
+		[HarmonyPostfix]
+		public static void Postfix(
+			ref List<ControllerIconData> ___currentIcons,
+			List<ControllerIconData> ___dualSenseControllerIcons,
+			List<ControllerIconData> ___dualShockControllerIcons,
+			List<ControllerIconData> ___xBoxControllerIcons,
+			List<ControllerIconData> ___joyConControllerIcons)
+		{
+			if (!LazyInput.IsGamepadActive) return;
+
+			// Apply the icon list selected in the config file
+			switch (Plugin.PreferredIcons.Value)
+			{
+				case Plugin.TargetIconType.DualSense:
+					___currentIcons = ___dualSenseControllerIcons;
+					break;
+				case Plugin.TargetIconType.DualShock:
+					___currentIcons = ___dualShockControllerIcons;
+					break;
+				case Plugin.TargetIconType.Xbox:
+					___currentIcons = ___xBoxControllerIcons;
+					break;
+				case Plugin.TargetIconType.JoyCon:
+					___currentIcons = ___joyConControllerIcons;
+					break;
+			}
+		}
+	}
+}
---
+++ Plugin.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ControllerIconSwapper
+{
+	[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+	public class Plugin : BaseUnityPlugin
+	{
+		internal static new ManualLogSource Logger;
+		// Define enum for the config dropdown choices
+		public enum TargetIconType
+		{
+			DualSense,
+			DualShock,
+			Xbox,
+			JoyCon
+		}
+
+		public static ConfigEntry<TargetIconType> PreferredIcons;
+
+		private void Awake()
+		{
+			// Plugin startup logic
+			Logger = base.Logger;
+			Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+			// Bind the config setting (creates BepInEx/config/com.yourname.dualsenseicons.cfg)
+			PreferredIcons = Config.Bind(
+				"General",                       // Section
+				"TargetIconType",                // Key
+				TargetIconType.DualShock,        // Default value
+				"Select which controller icons to show when a gamepad is active." // Description
+			);
+
+			var _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+			_harmony.PatchAll();
+		}
+	}
+}
